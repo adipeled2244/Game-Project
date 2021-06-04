1f31a0d098b47b89145f1f17e6028fd9fb6405b9
adi peled <adipeled224@gmail.com>
2021-06-04 19:42:49 +0300

    Final

---
+++ main.cpp
@@ -0,0 +1,12 @@
+#include <iostream>
+#include "boardGame.h"
+#include "GameController.h"
+#include <vector>
+
+int main(int argc, char const *argv[])
+{
+    GameController g;
+    g.play();
+    return 0;
+}
+
